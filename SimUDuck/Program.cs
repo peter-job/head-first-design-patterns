81513cc14e44ddb0932778e279552f0732b6af2c
Peter <ptjob6@gmail.com>
2019-07-28 15:09:41 +0100

    Setup solution with SimUDuck project

---
+++ SimUDuck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimUDuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
