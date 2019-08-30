7581fad4176c9ef3737f660e5e28424fadc36dda
Peter <ptjob6@gmail.com>
2019-08-31 00:21:43 +0100

    Complete ChocolateBoiler singleton project

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,55 @@
+﻿namespace ChocolateBoiler
+{
+    public class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        private static ChocolateBoiler uniqueInstance;
+
+        private ChocolateBoiler()
+        {
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new ChocolateBoiler();
+            }
+            return uniqueInstance;
+        }
+
+        public void Fill() {
+            if (IsEmpty()) {
+                empty = false;
+                boiled = false;
+            }
+        }
+
+        public void Drain() {
+            if (IsEmpty() && IsBoiled())
+            {
+                empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                boiled = true;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return boiled;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ChocolateBoiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boiler1 = ChocolateBoiler.GetInstance();
+            var boiler2 = ChocolateBoiler.GetInstance();
+
+            if (!boiler1.IsBoiled()) {
+                Console.WriteLine("Boiler 1 is cold");
+            }
+            if (!boiler2.IsBoiled()) {
+                Console.WriteLine("Boiler 2 is cold");
+            }
+            Console.WriteLine("Turning on Boiler 1...");
+            boiler1.Boil();
+            if (boiler1.IsBoiled())
+            {
+                Console.WriteLine("Boiler 1 is boiled!");
+            }
+            if (boiler2.IsBoiled())
+            {
+                Console.WriteLine("So is Boiler 2!");
+            }
+
+            if (boiler1 == boiler2)
+            {
+                Console.WriteLine("GetInstance returns the same chocolate boiler each time!");
+            }
+            else
+            {
+                Console.WriteLine("GetInstance returns different chocolate boilers each time!");
+            }
+        }
+    }
+}
